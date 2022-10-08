eefdca2074ce515449f6e52bf910067938de1c3c
olgatut <lelka.tutskaya@yandex.ru>
2022-10-08 23:50:08 +0300

    Домашнее задание Условный оператор 2

---
+++ src/Main.java
@@ -0,0 +1,99 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+    }
+    private static void task1() {
+        System.out.println("ЗАДАЧА №1 версия 1");
+        System.out.println(" ");
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке: https://play.google.com/store/apps/details?id=com.namsung.xgps160&hl=ru&gl=US");
+        }
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке: https://apps.apple.com/ru/app/skypro-%D0%BE%D0%BD%D0%BB%D0%B0%D0%B9%D0%BD-%D1%83%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D1%82%D0%B5%D1%82/id1568515479");
+        }
+        System.out.println();
+    }
+    private static void task2() {
+        System.out.println("ЗАДАЧА №1 версия 2");
+        System.out.println(" ");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке: https://play.google.com/store/apps/details?id=com.namsung.xgps160&hl=ru&gl=US");
+        } else {
+            if (clientOS == 1) {
+                System.out.println("Установите версию приложения для Android по ссылке: https://apps.apple.com/ru/app/skypro-%D0%BE%D0%BD%D0%BB%D0%B0%D0%B9%D0%BD-%D1%83%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D1%82%D0%B5%D1%82/id1568515479");
+            }
+        }
+        System.out.println(" ");
+    }
+    private static void task3() {
+        System.out.println("ЗАДАЧА №1 версия 3");
+        System.out.println(" ");
+        int clientOS = 3;
+        switch (clientOS) {
+            case 0:
+                //ссылка не рабочая
+                System.out.println("Установите версию приложения для IOS по ссылке: https://play.google.com/store/apps");
+                break;
+            case 1:
+                //ссылка не рабочая
+                System.out.println("Установите версию приложения для Android по ссылке: https://apps.apple.com/ru/app/skypro");
+                break;
+            case 2:
+            default:
+                System.out.println("Eror 404.");
+        }
+        System.out.println(" ");
+        }
+    private static void task4() {
+        System.out.println("ЗАДАЧА №2");
+        System.out.println(" ");
+        //«Установите облегченную версию приложения для iOS по ссылке».
+        //«Установите облегченную версию приложения для Android по ссылке».
+        int clientDeviceYear = 2015;
+        int software = 0;
+        String linkAndroid = "https://play.google.com/store/apps/details?id=skyeng.words.prod&hl=ru&gl=US";
+        String linkIos = "https://apps.apple.com/ru/app/skypro-%D0%BE%D0%BD%D0%BB%D0%B0%D0%B9%D0%BD-%D1%83%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D1%82%D0%B5%D1%82/id1568515479";
+        if(clientDeviceYear <= 2015){
+            if(software == 1){
+                System.out.printf("Установите облегченную версию приложения для Android по ссылке: %s ", linkAndroid);
+            }
+					else{
+                System.out.printf("Установите облегченную версию приложения для IOS по ссылке: %s ", linkIos);
+            }
+        } else{
+            if(software == 1){
+                System.out.printf("Установите приложение для Android по ссылке: %s ", linkAndroid);
+            }
+					else{
+                System.out.printf("Установите приложение для IOS по ссылке: %s ", linkIos);
+            }
+            }
+        System.out.println(" ");
+        System.out.println(" ");
+    }
+    private static void task5() {
+        System.out.println("ЗАДАЧА №3");
+        System.out.println(" ");
+        int year = 2000;
+        String yearPrint = String.valueOf(year);
+        if (year % 4 == 0 || year % 100 == 0 || year % 400 == 0){
+            System.out.printf("%s год является високостным", yearPrint);
+        } else{
+            System.out.printf("%s год не является високостным", yearPrint);
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+    }
+    private static void task6() {
+        System.out.println("ЗАДАЧА №4");
+        System.out.println(" ");
+
+        }
+}
